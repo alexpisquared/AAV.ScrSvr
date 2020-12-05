293d97910a2ff673289ea436f460e50372cbdc92
AlexPi <alex.pigida@outlook.com>
2020-12-05 16:46:24 -0500

    Control panel immprovements

--- Src/AlexPi.Scr/AlexPi.Scr/Vws/ControlPanel.xaml.cs
+++ Src/AlexPi.Scr/AlexPi.Scr/Vws/ControlPanel.xaml.cs
@@ -38,35 +38,66 @@ namespace AlexPi.Scr.Vws
     }
     async void onPreviewKeyUp(object s, KeyEventArgs e)
     {
+      await App.SpeakAsync($"CP: {e.Key} ");
       switch (e.Key)
       {
-        case Key.F12:
-          if (cbAll.IsChecked == true)      /**/ cbAll.IsChecked = false;
-          else if (cbAll.IsChecked == null) /**/ cbAll.IsChecked = true;
-          else                              /**/ cbAll.IsChecked = null;
-          onTglAll(s, null); goto case Key.None;
-        case Key.None:
+        //case Key.F12:
+        //  if (cbAll.IsChecked == true)      /**/ cbAll.IsChecked = false;
+        //  else if (cbAll.IsChecked == null) /**/ cbAll.IsChecked = true;
+        //  else                              /**/ cbAll.IsChecked = null;
+        //  onTglAll(s, null); goto case Key.None;
         case Key.NumPad0:
+        case Key.NumPad1:
+        case Key.NumPad2:
+        case Key.NumPad3:
+        case Key.NumPad4:
+        case Key.NumPad5:
+        case Key.NumPad6:
+        case Key.NumPad7:
         case Key.NumPad8:
         case Key.NumPad9:
         case Key.D0:
+        case Key.D1:
+        case Key.D2:
+        case Key.D3:
+        case Key.D4:
+        case Key.D5:
+        case Key.D6:
+        case Key.D7:
         case Key.D8:
         case Key.D9:
+          break;
 
-        case Key.F1: _GlobalEventHandler.TglContainerVis(ContainerA); break;
-        case Key.F2: _GlobalEventHandler.TglContainerVis(ContainerB); break;
-        case Key.F3: _GlobalEventHandler.TglContainerVis(ContainerC); break;
+        case Key.F1: _GlobalEventHandler.TglContainerVis(ContainerA); goto case Key.None;
+        case Key.F2: _GlobalEventHandler.TglContainerVis(ContainerB); goto case Key.None;
+        case Key.F3: _GlobalEventHandler.TglContainerVis(ContainerC); goto case Key.None;
+        case Key.F4: _GlobalEventHandler.TglContainerVis(ContainerD); goto case Key.None;
+        case Key.F5: _GlobalEventHandler.TglContainerVis(ContainerE); goto case Key.None;
+        case Key.F6: _GlobalEventHandler.TglContainerVis(ContainerF); goto case Key.None;
+        case Key.F7: _GlobalEventHandler.TglContainerVis(ContainerG); goto case Key.None;
+        case Key.F8: _GlobalEventHandler.TglContainerVis(ContainerH); goto case Key.None;
+        case Key.F9: _GlobalEventHandler.TglContainerVis(ContainerI); goto case Key.None;
+        case Key.F10: _GlobalEventHandler.TglContainerVis(ContainerJ); goto case Key.None;
+        case Key.F11: _GlobalEventHandler.TglContainerVis(ContainerK); goto case Key.None;
+        case Key.F12: _GlobalEventHandler.TglContainerVis(ContainerL); goto case Key.None;
+        case Key.None: await App.SpeakAsync($"Toggled."); break;
 
-        case Key.F9:
-        case Key.F10:
-        case Key.F11: await App.SpeakAsync($"{e.Key} is in the list. Handled set to true."); break;
-        default: await App.SpeakAsync($"{e.Key} is handled by the default case, but handled is still false!"); return;
+        default:/**/ await App.SpeakAsync($"Handled set to false!"); return;
       }
 
       e.Handled = true;
+      await App.SpeakAsync($"Handled set to true.");
     }
     void onChkUnC(object s, RoutedEventArgs e) => _GlobalEventHandler.TglContainerVis(((FrameworkElement)s).Name, (((ToggleButton)s).IsChecked == true));//var vis = boo ? Visibility.Visible : Visibility.Collapsed;//switch (((CheckBox)s).Name)//{//  case "ContainerA": ((App)Application.Current).CntrA.Visibility = vis; AppSettings.Instance.CtrlA = boo; break;//  case "ContainerB": ((App)Application.Current).CntrB.Visibility = vis; AppSettings.Instance.CtrlB = boo; break;//  case "ContainerC": ((App)Application.Current).CntrC.Visibility = vis; AppSettings.Instance.CtrlC = boo; break;//  case "ContainerD": ((App)Application.Current).CntrD.Visibility = vis; AppSettings.Instance.CtrlD = boo; break;//  case "ContainerE": ((App)Application.Current).CntrE.Visibility = vis; AppSettings.Instance.CtrlE = boo; break;//  case "ContainerF": ((App)Application.Current).CntrF.Visibility = vis; AppSettings.Instance.CtrlF = boo; break;//  case "ContainerG": ((App)Application.Current).CntrG.Visibility = vis; AppSettings.Instance.CtrlG = boo; break;//  case "ContainerH": ((App)Application.Current).CntrH.Visibility = vis; AppSettings.Instance.CtrlH = boo; break;//  case "ContainerI": ((App)Application.Current).CntrI.Visibility = vis; AppSettings.Instance.CtrlI = boo; break;//  case "ContainerJ": ((App)Application.Current).CntrJ.Visibility = vis; AppSettings.Instance.CtrlJ = boo; break;//  case "ContainerK": ((App)Application.Current).CntrK.Visibility = vis; AppSettings.Instance.CtrlK = boo; break;//  default://    break;//}
-    void onTglAll(object s, RoutedEventArgs e) { switch (cbAll.IsChecked) { case true: cacheCurVals(); setViz_And_ChkBoxVal(true); break; case false: cacheCurVals(); setViz_And_ChkBoxVal(false); break; default: restoreCachedVals(); break; } }
+    void onTglAll(object s, RoutedEventArgs e)
+    {
+      switch (cbAll.IsChecked)
+      {
+        case true: cacheCurVals(); setViz_And_ChkBoxVal(true); break;
+        case false: cacheCurVals(); setViz_And_ChkBoxVal(false); break;
+        default: restoreCachedVals(); break;
+      }
+    }
 
     // public static readonly DependencyProperty IsOnProperty = DependencyProperty.Register("IsOn", typeof(bool), typeof(ControlPanel)/*, new PropertyMetadata(true, pcc)*/); public bool IsOn { get { return (bool)GetValue(IsOnProperty); } set { SetValue(IsOnProperty, value); } } static void pcc(DependencyObject d, DependencyPropertyChangedEventArgs e) => ((App)App.Current).CntrA.Visibility = (bool)e.NewValue ? Visibility.Visible : Visibility.Collapsed;
 
