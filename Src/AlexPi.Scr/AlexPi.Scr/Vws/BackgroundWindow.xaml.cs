35e79c3e5dc0c9b969f350882363850243d46d6a
AlexPi <alex.pigida@outlook.com>
2021-02-11 21:51:11 -0500

    color

--- Src/AlexPi.Scr/AlexPi.Scr/Vws/BackgroundWindow.xaml.cs
+++ Src/AlexPi.Scr/AlexPi.Scr/Vws/BackgroundWindow.xaml.cs
@@ -34,7 +34,7 @@ namespace AlexPi.Scr.Vws
         WindowState = WindowState.Normal;
         Width = trgScreen.Bounds.Width;
         Height = 50; //  trgScreen.Bounds.Height / 32;
-        //Background = Brushes.DarkCyan;
+        Background = Brushes.DarkViolet;
         Top = trgScreen.Bounds.Top + 40; // task bar considerations.
         //Opacity = .75;
       }
