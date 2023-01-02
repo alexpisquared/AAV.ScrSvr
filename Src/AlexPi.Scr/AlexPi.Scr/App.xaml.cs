304f4f49d5be56dfe8f0ba484ea1f33b4a95a54d
AlexPi <alex.pigida@outlook.com>
2023-01-01 21:21:57 -0500

    ++

--- Src/AlexPi.Scr/AlexPi.Scr/App.xaml.cs
+++ Src/AlexPi.Scr/AlexPi.Scr/App.xaml.cs
@@ -1,6 +1,4 @@
-﻿using AmbienceLib;
-
-namespace AlexPi.Scr;
+﻿namespace AlexPi.Scr;
 public partial class App : Application
 {
   readonly GlobalEventHandler _globalEventHandler = new();
@@ -13,10 +11,7 @@ public partial class App : Application
 
   static readonly ushort _volume = (ushort)(DateTime.Now.Hour is > 8 and < 21 ? ushort.MaxValue : ushort.MaxValue / 16);
   static readonly string[] _voices = new[] { CC.UkuaPolinaNeural.Voice, CC.ZhcnXiaomoNeural.Voice, CC.EnusAriaNeural.Voice, CC.EngbSoniaNeural.Voice, CC.EngbRyanNeural.Voice };
-  static readonly SpeechSynth _synth = new("bdefa0157d1", true,
-    Environment.UserName.ToLower().StartsWith("j") ? CC.ZhcnXiaomoNeural.Voice :
-    Environment.UserName.ToLower().StartsWith("a") ? _voices[DateTime.Now.Second % _voices.Length] : 
-                                                /**/ CC.EnusAriaNeural.Voice);  
+  static readonly SpeechSynth _synth = new("bdefa0157d1", true, Environment.UserName.ToLower().StartsWith("j") ? CC.ZhcnXiaomoNeural.Voice : Environment.UserName.ToLower().StartsWith("a") ? _voices[DateTime.Now.Second % _voices.Length] : CC.EnusAriaNeural.Voice);  
   static readonly object _thisLock = new();
   static bool _mustLogEORun = false;
   public static readonly DateTime StartedAt = DateTime.Now;
