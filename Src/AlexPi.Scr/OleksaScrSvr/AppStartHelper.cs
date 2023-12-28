f3e65934a3c11007601d4c751a51df911b0863f4
alexpisqueared <alex.pigida@outlook.com>
2023-12-28 15:20:36 -0500

    ++

--- Src/AlexPi.Scr/OleksaScrSvr/AppStartHelper.cs
+++ Src/AlexPi.Scr/OleksaScrSvr/AppStartHelper.cs
@@ -5,7 +5,7 @@ public static class AppStartHelper
   {
     _ = services.AddSingleton<IConfigurationRoot>(new ConfigRandomizer("appsettings.OleksaScrSvr.json").Config);
 
-    _ = services.AddSingleton<ILogger>(sp => SeriLogHelper.InitLoggerFactory(@$"{Path.Combine(OneDrive.Root, @"Public")}\Logs\{Assembly.GetExecutingAssembly().GetName().Name}.{Environment.MachineName[..3]}.{Environment.UserName[..3]}..log", "-Info -Verb +Infi").CreateLogger<MainNavView>());
+    _ = services.AddSingleton<ILogger>(sp => SeriLogHelper.InitLoggerFactory(@$"{Path.Combine(OneDrive.Root, @"Public")}\Logs\{Assembly.GetExecutingAssembly().GetName().Name}.{Environment.MachineName[..3]}.{Environment.UserName[..3]}..log", sp.GetRequiredService<IConfigurationRoot>()["MinimumLogLevel"] ?? "+Info -Verb +Infi").CreateLogger<MainNavView>());
 
     _ = services.AddSingleton<IBpr, Bpr>(); // _ = VersionHelper.IsDbgAndRBD ? services.AddSingleton<IBpr, Bpr>() : services.AddSingleton<IBpr, BprSilentMock>();
 
@@ -14,7 +14,7 @@ public static class AppStartHelper
     _ = services.AddSingleton<SpeechSynth>(sp => SpeechSynth.Factory(
       sp.GetRequiredService<IConfigurationRoot>()["AppSecrets:MagicSpeech"] ?? throw new ArgumentNullException("\"AppSecrets:MagicSpeech\" ia not available."),
       sp.GetRequiredService<ILogger>()));
-    
+
     OpenWeaWpfApp.AppStartHelper.InitOpenWeaServices(services);
   }
 }
