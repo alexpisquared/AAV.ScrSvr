b00689e4808372b4882e632dc065e92629c736a4
alexpisqueared <alex.pigida@outlook.com>
2024-01-23 19:28:22 -0500

    ++

--- Src/AlexPi.Scr/PostScrSvr/MainWindow.xaml.cs
+++ Src/AlexPi.Scr/PostScrSvr/MainWindow.xaml.cs
@@ -9,9 +9,10 @@ public partial class MainWindow : Window
   public string videoFilename
   {
     get {
-      if (dg1.SelectedItem == null) return @"C:\Users\alexp\OneDrive\Pictures\id.png";
       Trace.WriteLine($"** {((VideoLogEntry)dg1.SelectedItem).VideoFilename}");
-      return @"C:\Users\alexp\OneDrive" + ((VideoLogEntry)dg1.SelectedItem).VideoFilename;
+      return dg1.SelectedItem == null
+        ? @"C:\Users\alexp\OneDrive\Pictures\id.png"
+        : @"C:\Users\alexp\OneDrive" + ((VideoLogEntry)dg1.SelectedItem).VideoFilename;
     }
   }
 
@@ -20,9 +21,22 @@ public partial class MainWindow : Window
   async void OnLoadedAsync(object s, RoutedEventArgs e)
   {
     var list = await File.ReadAllLinesAsync("""C:\Users\alexp\OneDrive\Public\Logs\OleksaScrSvr.Video.log""");
-    var entries = list.Select(x => x.Split('\t')).Select(x => new VideoLogEntry(DateTimeOffset.Parse(x[0]), DateTimeOffset.Parse(x[1]), int.Parse(x[2]), x[3], x[4], x[5], x[6])).ToList();
+    var entries = list.Select(x => x.Split('\t')).Select(x => SafeCreateRecord(x)
+    ).ToList();
 
     dg1.ItemsSource = entries.OrderByDescending(r => r.Displayed);
+  }
+
+  static VideoLogEntry SafeCreateRecord(string[] x)
+  {
+    try
+    {
+      return new VideoLogEntry(DateTimeOffset.Parse(x[0]), DateTimeOffset.Parse(x[1]), int.Parse(x[2]), x[3], x[4], x[5], x[6]);
+    }
+    catch (Exception)
+    {
+      return new VideoLogEntry(DateTimeOffset.Now, DateTimeOffset.Now, 0, "·", "5qGo7te", "\\Pictures\\Main\\2016\\2012-05-04 - 2016-04-23 iPod\\IMG_6015.JPG", "https://dsm04pap002files.storage.live.com/y4mp2lNt4HhzhVmtpgKh-uPicy49p3x877F1fVep38LbXGcTaUMKtFE52eDbBNVfEDN1Z-nKLPtYQb8mX8N3aiOd-MvALZO8npVEgZQxyw7B_8QCbaCfezULNtl-0WsHF6ugr7wvBqb1txEsZhXDBVC0gS0UlCnyLaEyfgHyGFFx3fjmlLM_niMJ5mqC768WI6AJEm5e9PtS0jagp83HwKmi-2NZVCqWccIn3I96JTHyuU?width=800&height=598&cropmode=none");
+    }
   }
 
   void dg1_SelectionChanged(object s, SelectionChangedEventArgs e) { mediaElement1.Source = new Uri(((VideoLogEntry)dg1.SelectedItem).ThumbnailUrl); ; }
