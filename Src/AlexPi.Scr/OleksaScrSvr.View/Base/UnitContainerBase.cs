0d77b3ac77f90d04ef37f25c6ed73880cc5e1454
alexpisqueared <alex.pigida@outlook.com>
2024-06-29 20:33:57 -0400

    ++

--- Src/AlexPi.Scr/OleksaScrSvr.View/Base/UnitContainerBase.cs
+++ Src/AlexPi.Scr/OleksaScrSvr.View/Base/UnitContainerBase.cs
@@ -24,7 +24,7 @@ public partial class UnitContainerBase : UserControl
 
   ILogger? _logger; public ILogger Logger => _logger ??= (DataContext as dynamic)?.Logger ?? SeriLogHelper.CreateLogger<UnitContainerBase>();
   string JsonFil => @$"\temp\_{Name}_{(DevOps.IsDbg ? "dbg" : "")}.json";
-  string JsonFile => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), @$"AppSettings\{AppDomain.CurrentDomain.FriendlyName}\_{Name}_{(VersionHelper.IsDbg ? "dbg" : "")}.json");
+  string JsonFile => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @$"AppSettings\{AppDomain.CurrentDomain.FriendlyName}\_{Name}_{(VersionHelper.IsDbg ? "dbg" : "")}.json");
 
   async Task StorePlacementBackToFile()
   {
