2f759285d0dce106164f00ee55edff4ffb2c8141
AlexPi <alex.pigida@outlook.com>
2025-05-15 13:52:32 -0400

    jki pau

--- Src/AlexPi.Scr/OleksaScrSvr.View/UnitF4View.xaml.cs
+++ Src/AlexPi.Scr/OleksaScrSvr.View/UnitF4View.xaml.cs
@@ -5,5 +5,22 @@ public partial class UnitF4View
   public UnitF4View() => InitializeComponent();
   void onShow(object s, MouseEventArgs e) { } // this takes 2% of CPU: must be colliding with the Begin in XAML:  try { ((Storyboard)FindResource("PeriodicFlash")).Begin(); } catch (Exception ex) { tbk1.Text = ex.Message; } }
 
-  void OnMouseEnter(object sender, MouseEventArgs e) => Application.Current.Shutdown();     //Close();
+  void OnMouseEnter(object sender, MouseEventArgs e) => WriteLine("MouseEnter"); //  Application.Current.Shutdown();     //Close();
+
+  int count = 0;
+  void OnMouseMove(object sender, MouseEventArgs e)
+  {
+    if (++count < 3)
+    {
+      WriteLine($"{DateTime.Now:HH:mm:ss.fff}  {count,2}   MouseEnter   <   3");
+      Console.Beep(260, 60);
+      return;
+    }
+
+    WriteLine($"{DateTime.Now:HH:mm:ss.fff}  {count,2}   MouseEnter");
+    Console.Beep(1260, 160);
+    WriteLine($"{DateTime.Now:HH:mm:ss.fff}  {count,2}   MouseEnter   =>   Application.Current.Shutdown();     ");
+
+    Application.Current.Shutdown();     //Close();
+  }
 }
