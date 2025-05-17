8e038aee4aac6e0a0af7eb6b669377403fbb42f3
AlexPi <alex.pigida@outlook.com>
2025-05-16 20:39:40 -0400

    pau

--- Src/AlexPi.Scr/OleksaScrSvr/App.xaml.cs
+++ Src/AlexPi.Scr/OleksaScrSvr/App.xaml.cs
@@ -185,6 +185,7 @@ public partial class App : System.Windows.Application
                                                                                                                 {
                                                                                                                   await speech.SpeakAsync($"Sweet dreams.");
                                                                                                                   LogScrSvrUptimeOncePerSession("ScrSvr - Dn - PC sleep enforced by the screen saver."); ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// Log ScrSvr Uptime (___Idle) Once Per Session
+                                                                                                                  await Task.Delay(1_000); // give it a second to finish the speech.
 
                                                                                                                   var sleepStart = DateTimeOffset.Now;
                                                                                                                   logger.Log(LogLevel.Information, $"╞══{TimeSoFar} SetSuspendState(); ■ never?! goes beyond this on NUC2, GRAM1; only on RAZER1, MF1. On MF1 this is EOIdle  \n█·                     │");
