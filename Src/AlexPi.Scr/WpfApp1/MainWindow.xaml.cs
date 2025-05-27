65497bbcef44485d600583529aaec1a4cd6f88bd
AlexPi <alex.pigida@outlook.com>
2025-05-26 22:11:17 -0400

    ++++++

--- Src/AlexPi.Scr/WpfApp1/MainWindow.xaml.cs
+++ Src/AlexPi.Scr/WpfApp1/MainWindow.xaml.cs
@@ -1,37 +1,24 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace WpfApp1;
 public partial class MainWindow : Window
 {
-  public MainWindow()
-  {
-    InitializeComponent();
-  }
+  public MainWindow() => InitializeComponent();
 
   async void OnLoaded(object sender, RoutedEventArgs e)
   {
 
     tbk1.Text = $"{DateTime.Now:H:mm:ss.fffffff}";
 
+    const int periodInMinutes = 10;
     while (true)
     {
-      // update tbk1.Text periodically every 2 minutes but such that each update happens exactly on 0 second, like:  at 3:00:00.000 3:02:00.000 3:02:04.000 3:06:00.000, etc.
-      var currentTime = DateTime.Now;
-      var targetWait = TimeSpan.FromMinutes(2 - currentTime.Minute % 2).Subtract(currentTime- currentTime.AddSeconds(-currentTime.Second));
-      var targetTime = currentTime.Add(targetWait);
-      tbk1.Text = $"{currentTime:H:mm:ss.fffffff}\n{targetTime:H:mm:ss.fffffff}";
+      // update tbk1.Text periodically every periodInMinutes minutes but such that each update happens exactly on 0 second, like:  at 3:00:00.000 3:10:00.000 3:20:00.000 3:20:00.000, etc.
+      DateTime currentTime = DateTime.Now;
+      TimeSpan targetWait = TimeSpan.FromMinutes(periodInMinutes - (currentTime.Minute % periodInMinutes)).Subtract(currentTime - currentTime.AddSeconds(-currentTime.Second - 0.001 * currentTime.Millisecond - 0.000001 * currentTime.Microsecond));
+      DateTime targetTime = currentTime.Add(targetWait);
+      tbk1.Text = $"{currentTime:H:mm:ss.ffffff}\n{targetTime:H:mm:ss.ffffff}";
       await Task.Delay(targetWait);
     }
-
-
   }
 }
