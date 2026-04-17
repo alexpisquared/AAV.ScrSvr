9e1736966880114a1c464cf7d74aa7d7030e1975
AlexPi <alex.pigida@outlook.com>
2026-04-17 11:01:12 -0400

    Update MainWindow.xaml.cs

--- Src/AlexPi.Scr/TtsVoicesPocWpfApp/MainWindow.xaml.cs
+++ Src/AlexPi.Scr/TtsVoicesPocWpfApp/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using AmbienceLib;
 using Microsoft.Extensions.Configuration;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -16,11 +17,12 @@ public partial class MainWindow : Window
   async void OnSpeak(object sender, RoutedEventArgs e)
   {
     _synth.SpeakAsyncCancelAll();
-    await _synth.SpeakAsync(tbSpeech.Text, voice: GetTheVoiceMatchingButtonContentWhichIsPresumablyExactlyMatchesTheCC_Names(((Button)sender)?.Content?.ToString()));
+    await _synth.SpeakAsync(tbSpeech.Text, voice: GetTheVoiceMatchingButtonContentWhichIsPresumablyExactlyMatchesTheCC_Names(((Button)sender)?.Content?.ToString()?.Replace("_", "")));
   }
 
-  static string GetTheVoiceMatchingButtonContentWhichIsPresumablyExactlyMatchesTheCC_Names(string? voice)
-  {
-    return voice ?? SpeechSynth.CC.Polina;
-  }
+  static string GetTheVoiceMatchingButtonContentWhichIsPresumablyExactlyMatchesTheCC_Names(string? voice) =>
+    typeof(SpeechSynth.CC)
+      .GetField(voice ?? "", BindingFlags.Public | BindingFlags.Static)?
+      .GetValue(null) as string
+    ?? SpeechSynth.CC.Polina;
 }
