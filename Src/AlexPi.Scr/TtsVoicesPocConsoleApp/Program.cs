3f7149e12f77618cb62a973d428bbf13dc572fbd
AlexPi <alex.pigida@outlook.com>
2026-04-16 23:01:59 -0400

    ssi

--- Src/AlexPi.Scr/TtsVoicesPocConsoleApp/Program.cs
+++ Src/AlexPi.Scr/TtsVoicesPocConsoleApp/Program.cs
@@ -1,52 +1,12 @@
-﻿using AmbienceLib;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Threading.Tasks;
-//using AmbienceLib;
-//using Microsoft.Extensions.Logging;
+﻿Console.WriteLine("Beginning ...");
 
-/// <summary>
-/// Apparently, 
-///   ethnic voices can read their language and a very broken English
-///   Suffix RUS does not read cyrillic at all: it is skipped altogether, while English is read OK.
-/// </summary>
-
-Console.WriteLine("Beginning ...");
-
-try
-{
-  Console.WriteLine($"          domain : {System.DirectoryServices.ActiveDirectory.Domain.GetComputerDomain()}");
-}
-catch (Exception ex)
-{
-  Console.WriteLine($"          domain : {ex.Message}");
-}
-
-//await Test2();
-//await Test3();
+//try
+//{
+//  Console.WriteLine($"          domain : {System.DirectoryServices.ActiveDirectory.Domain.GetComputerDomain()}");
+//}
+//catch (Exception ex)
+//{
+//  Console.WriteLine($"          domain : {ex.Message}");
+//}
 
 Console.WriteLine("          ... the end!");
-
-public class SpeechSynth2 : SpeechSynth, IDisposable
-{
-  public SpeechSynth2(string speechKey, bool useCached = true, string voice = "en-US-AriaNeural", string speechSynthesisLanguage = "uk-UA", ILogger lgr = null, string pathToCache = "C:\\g\\AAV.Shared\\Src\\NetLts\\Ambience\\MUMsgs\\") : base(speechKey, useCached, voice, speechSynthesisLanguage, lgr, pathToCache)
-  {
-  }
-
-  public void Dispose() => throw new NotImplementedException();
-
-  public async Task<bool> test1()
-  {
-    using var _synth = new SpeechSynth("");
-    var s = "Hi there?";
-    await _synth.SpeakAsync(s, 1, 100, "es-ES-HelenaRUS");
-    await _synth.SpeakAsync(s, 1, 100, "es-ES-HelenaRUS");
-
-    //await _synth.SpeakAsync(s, a, "uk-UA-OstapNeural");
-    //await _synth.SpeakAsync(s, a, "uk-UA-OstapNeural");  // bad ending
-    //await _synth.SpeakAsync(s, a, "uk-UA-PolinaNeural"); // the best
-    //await _synth.SpeakAsync(s, a, "uk-UA-OstapNeural");  // best question intonation
-
-    return true;
-  }
-}
---
+++ Src/AlexPi.Scr/TtsVoicesPocWpfApp/SpeechSynthTest.cs
@@ -0,0 +1,26 @@
+﻿using AmbienceLib;
+using Microsoft.Extensions.Logging;
+
+public class SpeechSynthTest : SpeechSynth, IDisposable
+{
+  public SpeechSynthTest(string speechKey, bool useCached = true, string voice = "en-US-AriaNeural", string speechSynthesisLanguage = "uk-UA", ILogger? lgr = null, string pathToCache = "C:\\g\\AAV.Shared\\Src\\NetLts\\Ambience\\MUMsgs\\") : base(speechKey, useCached, voice, speechSynthesisLanguage, lgr, pathToCache)
+  {
+  }
+
+  public new void Dispose() => throw new NotImplementedException();
+
+  public async Task<bool> test1()
+  {
+    using var _synth = new SpeechSynth("");
+    var s = "Hi there?";
+    await _synth.SpeakAsync(s, 1, 100, "es-ES-HelenaRUS");
+    await _synth.SpeakAsync(s, 1, 100, "es-ES-HelenaRUS");
+
+    //await _synth.SpeakAsync(s, a, "uk-UA-OstapNeural");
+    //await _synth.SpeakAsync(s, a, "uk-UA-OstapNeural");  // bad ending
+    //await _synth.SpeakAsync(s, a, "uk-UA-PolinaNeural"); // the best
+    //await _synth.SpeakAsync(s, a, "uk-UA-OstapNeural");  // best question intonation
+
+    return true;
+  }
+}
